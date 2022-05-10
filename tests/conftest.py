e37b055ad05d51fa16dcf5a1d0f31a65c3b7c6c0
Garry <gledford@users.noreply.github.com>
2022-05-10 09:02:28 -0400

    Initial Commit

---
+++ tests/conftest.py
@@ -0,0 +1,67 @@
+import os
+import pytest
+from app import app
+
+from tests import site as db
+
+
+TESTDB = 'site.db'
+TESTDB_PATH = "/opt/project/data/{}".format(TESTDB)
+TEST_DATABASE_URI = 'sqlite:///' + TESTDB_PATH
+
+
+@pytest.fixture(scope='session')
+def app(request):
+    """Session-wide test `Flask` application."""
+    settings_override = {
+        'TESTING': True,
+        'SQLALCHEMY_DATABASE_URI': TEST_DATABASE_URI
+    }
+    app = app
+
+    # Establish an application context before running the tests.
+    ctx = app.app_context()
+    ctx.push()
+
+    def teardown():
+        ctx.pop()
+
+    request.addfinalizer(teardown)
+    return app
+
+
+@pytest.fixture(scope='session')
+def db(app, request):
+    """Session-wide test database."""
+    if os.path.exists(TESTDB_PATH):
+        os.unlink(TESTDB_PATH)
+
+    def teardown():
+        db.drop_all()
+        os.unlink(TESTDB_PATH)
+
+    db.app = app
+    db.create_all()
+
+    request.addfinalizer(teardown)
+    return db
+
+
+@pytest.fixture(scope='function')
+def session(db, request):
+    """Creates a new database session for a test."""
+    connection = db.engine.connect()
+    transaction = connection.begin()
+
+    options = dict(bind=connection, binds={})
+    session = db.create_scoped_session(options=options)
+
+    db.session = session
+
+    def teardown():
+        transaction.rollback()
+        connection.close()
+        session.remove()
+
+    request.addfinalizer(teardown)
+    return session
